0da7697f208e61f0065c29902bc6a64aae47caf0
bhadresh-goswami <tops.bhadresh@gmail.com>
2019-01-03 18:51:05 +0530

    uploaded

--- structuresExample/main.swift
+++ structuresExample/main.swift
@@ -8,5 +8,33 @@
 
 import Foundation
 
-print("Hello, World!")
+//collecting information and process it in case of any operation performed that supported by structure
+
+struct Student {
+
+    public var no:Int
+    private var collegeName:String
+    public var name:String
+
+    init() {
+        print ("hi!")
+        no = 0
+        name = ""
+        collegeName = "abc"
+    }
+
+    mutating func SetData(no:Int, name:String)  {
+        self.no = no
+        self.name = name
+    }
+    func ShowDat()  {
+        print(no)
+        print(name)
+    }
+}
+
+var std1:Student = Student()//(no:1,name:"bhadresh")
+
+std1.SetData(no: 1, name: "bhadres")
+std1.ShowDat()
 
