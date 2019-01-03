5460b06af96865ccf1b49cb283548a39727e8eff
bhadresh-goswami <tops.bhadresh@gmail.com>
2019-01-03 18:43:35 +0530

    Initial Commit

---
+++ structuresExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  structuresExample
+//
+//  Created by Mac on 03/01/19.
+//  Copyright © 2019 Bhadresh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
